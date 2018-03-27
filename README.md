67a8c40882f8d89504673f05fd85ce305f4b3d90
bobosheep <ianpaul32@gmail.com>
2018-03-28 01:34:28 +0800

    add README and fix parameter

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # portAudio-practice
+-----------------------------
+## Intro
+*   try to do pitch shifting on Linux
+## Reference
+*   **portAudio**[http://portaudio.com/docs/v19-doxydocs/index.html](http://portaudio.com/docs/v19-doxydocs/index.html)
+*   **pitchShift**[http://blogs.zynaptiq.com/bernsee/repo/smbPitchShift.cpp](http://blogs.zynaptiq.com/bernsee/repo/smbPitchShift.cpp)
+
--- myRecord.c
+++ myRecord.c
@@ -8,7 +8,7 @@
 /* #define SAMPLE_RATE  (17932) // Test failure to open with this value. */
 #define SAMPLE_RATE  (44100)
 #define FRAMES_PER_BUFFER (512)
-#define NUM_SECONDS     (5)
+#define NUM_SECONDS     (10)
 #define NUM_CHANNELS    (2)
 /* #define DITHER_FLAG     (paDitherOff) */
 #define DITHER_FLAG     (0) /**/
@@ -440,143 +440,147 @@ int main(void)
     int                 numBytes;
     SAMPLE              max, val;
     double              average;
+    char                option;
 
     printf("myRecord.c\n"); fflush(stdout);
-
-    data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
-    data.frameIndex = 0;
-    numSamples = totalFrames * NUM_CHANNELS;
-    numBytes = numSamples * sizeof(SAMPLE);
-    data.recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
-    if( data.recordedSamples == NULL )
+    while(scanf("%c", &option) == 1 && option != 'q')
     {
-        printf("Could not allocate record array.\n");
-        goto done;
-    }
-    for( i=0; i<numSamples; i++ ) data.recordedSamples[i] = 0;
-
-    err = Pa_Initialize();
-    if( err != paNoError ) goto done;
-
-    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
-    if (inputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default input device.\n");
-        goto done;
-    }
-    inputParameters.channelCount = 2;                    /* stereo input */
-    inputParameters.sampleFormat = PA_SAMPLE_TYPE;
-    inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
-    inputParameters.hostApiSpecificStreamInfo = NULL;
-
-    /* Record some audio. -------------------------------------------- */
-    err = Pa_OpenStream(
-              &stream,
-              &inputParameters,
-              NULL,                  /* &outputParameters, */
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              recordCallback,
-              &data );
-    if( err != paNoError ) goto done;
-
-    err = Pa_StartStream( stream );
-    if( err != paNoError ) goto done;
-    printf("\n=== Now recording!! Please speak into the microphone. ===\n"); fflush(stdout);
-
-    while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
-    {
-        Pa_Sleep(1000);
-        printf("index = %d\n", data.frameIndex ); fflush(stdout);
-    }
-    if( err < 0 ) goto done;
-
-    err = Pa_CloseStream( stream );
-    if( err != paNoError ) goto done;
-
-    /* Measure maximum peak amplitude. */
-    max = 0;
-    average = 0.0;
-    tmp1 = data;
-    smbPitchShift(1.8, numSamples, 1024, 32, 44100, data.recordedSamples, tmp1.recordedSamples );
-    for( i=0; i<numSamples; i++ )
-    {
-        val = data.recordedSamples[i];
-        
-        if( val < 0 ) val = -val; /* ABS */
-        if( val > max )
+        data.maxFrameIndex = totalFrames = NUM_SECONDS * SAMPLE_RATE; /* Record for a few seconds. */
+        data.frameIndex = 0;
+        numSamples = totalFrames * NUM_CHANNELS;
+        numBytes = numSamples * sizeof(SAMPLE);
+        data.recordedSamples = (SAMPLE *) malloc( numBytes ); /* From now on, recordedSamples is initialised. */
+        if( data.recordedSamples == NULL )
         {
-            max = val;
+            printf("Could not allocate record array.\n");
+            goto done;
         }
-        average += val;
-        data.recordedSamples[i] *= 4;
-        tmp1.recordedSamples[i] *= 4;
-    }
+        for( i=0; i<numSamples; i++ ) data.recordedSamples[i] = 0;
 
-    average = average / (double)numSamples;
+        err = Pa_Initialize();
+        if( err != paNoError ) goto done;
 
-    printf("sample max amplitude = "PRINTF_S_FORMAT"\n", max );
-    printf("sample average = %lf\n", average );
-
-    /* Write recorded data to a file. */
-#if WRITE_TO_FILE
-    {
-        FILE  *fid;
-        fid = fopen("recorded.raw", "wb");
-        if( fid == NULL )
-        {
-            printf("Could not open file.");
+        inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+        if (inputParameters.device == paNoDevice) {
+            fprintf(stderr,"Error: No default input device.\n");
+            goto done;
         }
-        else
-        {
-            fwrite( data.recordedSamples, NUM_CHANNELS * sizeof(SAMPLE), totalFrames, fid );
-            fclose( fid );
-            printf("Wrote data to 'recorded.raw'\n");
-        }
-    }
-#endif
+        inputParameters.channelCount = 2;                    /* stereo input */
+        inputParameters.sampleFormat = PA_SAMPLE_TYPE;
+        inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
+        inputParameters.hostApiSpecificStreamInfo = NULL;
 
-    /* Playback recorded data.  -------------------------------------------- */
-    data.frameIndex = 0;
-    tmp2.frameIndex = 0;
-    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
-    if (outputParameters.device == paNoDevice) {
-        fprintf(stderr,"Error: No default output device.\n");
-        goto done;
-    }
-    outputParameters.channelCount = 2;                     /* stereo output */
-    outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
-    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
-    outputParameters.hostApiSpecificStreamInfo = NULL;
+        /* Record some audio. -------------------------------------------- */
+    
+        err = Pa_OpenStream(
+                &stream,
+                &inputParameters,
+                NULL,                  /* &outputParameters, */
+                SAMPLE_RATE,
+                FRAMES_PER_BUFFER,
+                paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                recordCallback,
+                &data );
+        if( err != paNoError ) goto done;
 
-    printf("\n=== Now playing back. ===\n"); fflush(stdout);
-    err = Pa_OpenStream(
-              &stream,
-              NULL, /* no input */
-              &outputParameters,
-              SAMPLE_RATE,
-              FRAMES_PER_BUFFER,
-              paClipOff,      /* we won't output out of range samples so don't bother clipping them */
-              playCallback,
-              &data );
-    if( err != paNoError ) goto done;
-
-    if( stream )
-    {
         err = Pa_StartStream( stream );
         if( err != paNoError ) goto done;
-        
-        printf("Waiting for playback to finish.\n"); fflush(stdout);
+        printf("\n=== Now recording!! Please speak into the microphone. ===\n"); fflush(stdout);
 
-        while( ( err = Pa_IsStreamActive( stream ) ) == 1 ) Pa_Sleep(10);
+        while( ( err = Pa_IsStreamActive( stream ) ) == 1 )
+        {
+            Pa_Sleep(1000);
+            printf("index = %d\n", data.frameIndex ); fflush(stdout);
+        }
         if( err < 0 ) goto done;
-        
+
         err = Pa_CloseStream( stream );
         if( err != paNoError ) goto done;
-        
-        printf("Done.\n"); fflush(stdout);
-    }
 
+        /* Measure maximum peak amplitude. */
+        max = 0;
+        average = 0.0;
+        tmp1 = data;
+        smbPitchShift(1.8, numSamples, 4096, 32, SAMPLE_RATE, data.recordedSamples, tmp1.recordedSamples );
+        for( i=0; i<numSamples; i++ )
+        {
+            val = data.recordedSamples[i];
+            
+            if( val < 0 ) val = -val; /* ABS */
+            if( val > max )
+            {
+                max = val;
+            }
+            average += val;
+            data.recordedSamples[i] *= 7;
+            tmp1.recordedSamples[i] *= 7;
+        }
+
+        average = average / (double)numSamples;
+
+        printf("sample max amplitude = "PRINTF_S_FORMAT"\n", max );
+        printf("sample average = %lf\n", average );
+
+        /* Write recorded data to a file. */
+    #if WRITE_TO_FILE
+        {
+            FILE  *fid;
+            fid = fopen("recorded.raw", "wb");
+            if( fid == NULL )
+            {
+                printf("Could not open file.");
+            }
+            else
+            {
+                fwrite( data.recordedSamples, NUM_CHANNELS * sizeof(SAMPLE), totalFrames, fid );
+                fclose( fid );
+                printf("Wrote data to 'recorded.raw'\n");
+            }
+        }
+    #endif
+
+        /* Playback recorded data.  -------------------------------------------- */
+        data.frameIndex = 0;
+        tmp2.frameIndex = 0;
+        outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+        if (outputParameters.device == paNoDevice) {
+            fprintf(stderr,"Error: No default output device.\n");
+            goto done;
+        }
+        outputParameters.channelCount = 2;                     /* stereo output */
+        outputParameters.sampleFormat =  PA_SAMPLE_TYPE;
+        outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+        outputParameters.hostApiSpecificStreamInfo = NULL;
+
+        printf("\n=== Now playing back. ===\n"); fflush(stdout);
+        err = Pa_OpenStream(
+                &stream,
+                NULL, /* no input */
+                &outputParameters,
+                SAMPLE_RATE,
+                FRAMES_PER_BUFFER,
+                paClipOff,      /* we won't output out of range samples so don't bother clipping them */
+                playCallback,
+                &data );
+        if( err != paNoError ) goto done;
+
+        if( stream )
+        {
+            err = Pa_StartStream( stream );
+            if( err != paNoError ) goto done;
+            
+            printf("Waiting for playback to finish.\n"); fflush(stdout);
+
+            while( ( err = Pa_IsStreamActive( stream ) ) == 1 ) Pa_Sleep(10);
+            if( err < 0 ) goto done;
+            
+            err = Pa_CloseStream( stream );
+            if( err != paNoError ) goto done;
+            
+            printf("Done.\n"); fflush(stdout);
+        }
+        printf("Press q to quit or other key to continue.\n"); fflush(stdout);
+    }
 done:
     Pa_Terminate();
     if( data.recordedSamples )       /* Sure it is NULL or valid. */
